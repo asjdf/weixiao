43219a749ad27010a4f35e2eb7605985ad50b48c
晓杰 <87080562+xiao-jay@users.noreply.github.com>
2021-12-02 20:44:10 +0800

    del():del the yaml

--- school/school.go
+++ school/school.go
@@ -1,11 +1,5 @@
 package school
 
-import (
-	"gopkg.in/yaml.v3"
-	"io/ioutil"
-	"log"
-)
-
 type Config struct {
 	AppKey    string
 	AppSecret string
@@ -22,16 +16,3 @@ func NewSchool(cfg *Config) *School {
 	return &School{conf: cfg, AccessTokenHandle: defaultAkHandle}
 }
 
-func GetConf() *Config {
-	c := new(Config)
-	yamlFile, err := ioutil.ReadFile("application.yaml")
-	if err != nil {
-		log.Printf("yamlFile.Get err   #%v ", err)
-	}
-
-	err = yaml.Unmarshal(yamlFile, &c)
-	if err != nil {
-		log.Fatalf("Unmarshal: %v", err)
-	}
-	return c
-}
