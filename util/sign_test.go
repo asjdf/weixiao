00a1ad5f09878981386577b8d80ed38ef63f5be3
晓杰 <87080562+xiao-jay@users.noreply.github.com>
2021-12-11 20:47:00 +0800

    (feat:composeCode)complete the composeCode :account and pass

--- util/sign_test.go
+++ util/sign_test.go
@@ -8,10 +8,9 @@ import (
 
 func TestSign(t *testing.T) {
 	values := url.Values{}
-	values.Add("b", "1234567890123456")
 	values.Add("app_key", "1234567890123456")
-	values.Add("nonce", "123456")
-	values.Add("timestamp", "1638186184435")
+	values.Add("timestamp", "123456")
+
 	values.Add("school_code", "41330103566")
 	sign := Sign("A5B59272FD2E95B21D02B9C9888104F0", values)
 	fmt.Println(sign)
